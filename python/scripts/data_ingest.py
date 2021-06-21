8366783de6c0d5a5bfe2dfea74293919380e5372
Kabilar Gunalan <kabilar@vathes.com>
2021-06-21 21:46:33 +0000

    Remove drop schemas

--- python/scripts/data_ingest.py
+++ python/scripts/data_ingest.py
@@ -211,30 +211,6 @@ pupil.FittedContour.populate()
 class ConfigDeeplabcut(dj.Manual):
 pupil.Tracking.populate()
 pupil.FittedPupil.populate()
-# -
-
-# # Drop schemas
-
-# + jupyter={"outputs_hidden": true}
-from pipeline import meso, reso, stack, treadmill, odor, mice, \
-                     experiment, shared, pupil, map, injection, \
-                     anatomy, lab, notify
-
-# + jupyter={"outputs_hidden": true}
-notify.schema.drop()
-pupil.schema.drop()
-stack.schema.drop()
-treadmill.schema.drop()
-odor.schema.drop()
-meso.schema.drop()
-reso.schema.drop()
-injection.schema.drop()
-anatomy.schema.drop()
-map.schema.drop()
-experiment.schema.drop()
-shared.schema.drop()
-mice.schema.drop()
-lab.schema.drop()
 
 # + jupyter={"outputs_hidden": true}
 
