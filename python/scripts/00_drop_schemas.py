97e54493f44cc8c5723cfc7833b9fdb9973fe1e7
Kabilar Gunalan <kabilar@vathes.com>
2021-06-21 21:57:52 +0000

    Rename file

---
+++ python/scripts/00_drop_schemas.py
@@ -0,0 +1,22 @@
+# This script is NOT required.
+# During the pipeline development phase it may be required to drop schemas.
+# If so, below is the dependency order.
+
+# from pipeline import meso, reso, stack, treadmill, odor, mice, \
+#                      experiment, shared, pupil, map, injection, \
+#                      anatomy, lab, notify
+
+# notify.schema.drop()
+# pupil.schema.drop()
+# stack.schema.drop()
+# treadmill.schema.drop()
+# odor.schema.drop()
+# meso.schema.drop()
+# reso.schema.drop()
+# injection.schema.drop()
+# anatomy.schema.drop()
+# map.schema.drop()
+# experiment.schema.drop()
+# shared.schema.drop()
+# mice.schema.drop()
+# lab.schema.drop()
