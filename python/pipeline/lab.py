b7406a24df414a25d18f89783bd2614af083fde6
Kabilar Gunalan <kabilar@vathes.com>
2021-06-28 15:44:39 +0000

    Update `lab.Paths`

--- python/pipeline/lab.py
+++ python/pipeline/lab.py
@@ -20,56 +20,13 @@ class Paths(dj.Lookup):
 
     def get_local_path(self, path, local_os=None):
 
-        # TODO keep or remove?
-        # determine local os
-        # if local_os is None:
-        #     local_os = sys.platform
-        #     local_os = local_os[:(min(3, len(local_os)))]
-        # if local_os.lower() == 'glo':
-        #     local = 0
-        #     home = '~'
-
-        # elif local_os.lower() == 'lin':
-        #     local = 1
-        #     home = os.environ['HOME']
-
-        # elif local_os.lower() == 'win':
-        #     local = 2
-        #     home = os.environ['HOME']
-
-        # elif local_os.lower() == 'dar':
-        #     local = 3
-        #     home = '~'
-
-        # else:
-        #     raise NameError('unknown OS')
-
-        # path = path.replace(os.path.sep, '/')
-        # path = path.replace('~', home)
-
-        # mapping = np.asarray(self.fetch('global', 'linux', 'windows', 'mac'))
-        # size = mapping.shape
-        # for i in range(size[1]):
-        #     for j in range(size[0]):
-        #         n = len(mapping[j, i])
-        #         if j != local and path[:n] == mapping[j, i][:n]:
-        #             path = os.path.join(mapping[local, i], path[n+1:])
-        #             break
-
-        # if os.path.sep == '\\' and local_os.lower() != 'glo':
-        #     path = path.replace('/', '\\')
-
-        # else:
-        #     path = path.replace('\\', '/')
-
-        # TODO implement with environment variables
         if path.split(':')[0] == 'L':
-            root_dir = '/data/external_scratch08'        
+            root_dir = os.environ.get('SCRATCH08_STORAGE')
         elif path.split(':')[0] == 'N':
-            root_dir = '/data/external_scratch10'
+            root_dir = os.environ.get('SCRATCH10_STORAGE')
+
         path = os.path.join(root_dir, path.split(':')[1])
         
         path = path.replace('\\', '/')
-        print(path)
 
         return path
